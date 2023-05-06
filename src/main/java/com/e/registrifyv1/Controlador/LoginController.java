2e9fa79c3db3cd6fc1a6683c25867852e92d193f
nscando <nicolas.scandizzo@gmail.com>
2023-05-06 20:18:03 -0300

    Login Validaciones

--- src/main/java/com/e/registrifyv1/Controlador/LoginController.java
+++ src/main/java/com/e/registrifyv1/Controlador/LoginController.java
@@ -1,15 +1,46 @@
 package com.e.registrifyv1.Controlador;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class LoginController {
    @FXML
-   private Label welcomeText;
+   private Label loginMensajeLabel;
+   @FXML
+   private Button salirBtn;
 
    @FXML
-   protected void onHelloButtonClick() {
-      welcomeText.setText("Welcome to JavaFX Application!");
+   private TextField usuarioTxtField;
+
+   @FXML
+   private PasswordField passwordTxtField;
+
+   @FXML
+   private Button ingresarBtn;
+
+
+   public void loginBtnAction(ActionEvent e) {
+
+      if(!usuarioTxtField.getText().isBlank() && !passwordTxtField.getText().isBlank()){
+         loginMensajeLabel.setText("Bievenido a Registrify!");
+
+      }else {
+         loginMensajeLabel.setText("Error en Usuario y/o Contraseña vuelva a intentar.");
+
+
+      }
+
+
+   }
+
+   public void salirBtnAction(ActionEvent e) {
+      Stage stage = (Stage) salirBtn.getScene().getWindow();
+      stage.close();
    }
 }
