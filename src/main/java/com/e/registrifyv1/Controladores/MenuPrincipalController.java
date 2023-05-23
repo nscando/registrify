29e5872ad8f102b9b60585fe929d92d356744c2f
nscando <nicolas.scandizzo@gmail.com>
2023-05-22 22:58:27 -0300

    UserView Interfaz

--- src/main/java/com/e/registrifyv1/Controladores/MenuPrincipalController.java
+++ src/main/java/com/e/registrifyv1/Controladores/MenuPrincipalController.java
@@ -1,4 +1,19 @@
 package com.e.registrifyv1.Controladores;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+
+
 public class MenuPrincipalController {
+
+   @FXML
+ private  Button btnUsuarioMenu;
+
+   public void btnMenuUsuarioAction()  {
+      // Cerrar la ventana actual
+
+   }
 }
+
