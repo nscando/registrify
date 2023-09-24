fb11b36fffd6386f4885e40ae8252ffb722fb69b
Patricio Ghirardi <ghirardi.patricio.w@gmail.com>
2023-09-24 17:34:44 -0300

    pryueba modulo vehiculos, controller.

---
+++ src/main/java/com/e/registrifyv1/Controladores/VehiculosMenuController.java
@@ -0,0 +1,23 @@
+package com.e.registrifyv1.Controladores;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import com.e.registrifyv1.Dao.UsuarioDAO;
+import com.e.registrifyv1.Modelos.Usuarios.UsuarioModel;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class VehiculosMenuController {
+    private void btBuscarAction(ActionEvent event) {
+       // por hacer funcion ??
+    }
+}
