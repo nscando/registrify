eb8a00aabbb68891753f44c52cd1bf0db78c4a9c
Patricio Ghirardi <ghirardi.patricio.w@gmail.com>
2024-07-27 00:44:14 -0300

    Seguimiento de  Backend MenuEvento Consulta, baja y carga funcionales Falta la modificacion.

--- src/main/java/com/e/registrifyv1/Controladores/Evento/ModificarEventoController.java
+++ src/main/java/com/e/registrifyv1/Controladores/Evento/ModificarEventoController.java
@@ -2,6 +2,7 @@ package com.e.registrifyv1.Controladores.Evento;
 import com.e.registrifyv1.Dao.EventoDAO;
 import  com.e.registrifyv1.Modelos.EventoDiario.EventoDiarioModel;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -13,16 +14,16 @@ public class ModificarEventoController {
     @FXML
     private Button btnCancelar;
 
-    //@FXML
-    //private TextField txbIdAccesorio;
-    @FXML
-    private TextField txbIdUnidad;
     @FXML
     private TextField txbIdGendarme;
     @FXML
-    private TextField txbNombre;
+    private TextField txbIdUnidad;
     @FXML
-    private TextField txbDescripcion;
+    private TextField txbDescripcionEvento;
+    @FXML
+    private TextField txbFechaEvento;
+    @FXML
+    private TextField txbEstadoEvento;
 
     private EventoDiarioModel evento;
 
@@ -41,27 +42,29 @@ public class ModificarEventoController {
 
     }
 
-    public void inicializarDatosModificacion(InventarioModel inventario){
-        this.inventario = inventario;
-        this.idInventarioSeleccionado = inventario.getIdAccesorio();//todo como funciona esto..?//antes estaba idGendarme
-        txbIdGendarme.setText(String.valueOf(inventario.getIdGendarme()));
-        txbIdUnidad.setText(String.valueOf(inventario.getIdUnidad()));
-        txbNombre.setText(inventario.getNombreAccesorio());
-        txbDescripcion.setText(inventario.getNombreAccesorio());
+    public void inicializarDatosModificacion(EventoDiarioModel evento){
+        this.evento = evento;
+        this.idEventoSeleccionado = evento.getIdEvento();
+        txbIdGendarme.setText(String.valueOf(evento.getIdGendarme()));
+        txbIdUnidad.setText(String.valueOf(evento.getIdUnidad()));
+        txbDescripcionEvento.setText(evento.getDescrEvento());
+        txbFechaEvento.setText(evento.getFechaEvento());
+        txbEstadoEvento.setText(evento.getEstado());// todo ???
+
     }
 
-    public void setTablaMenuInventario(TableView<InventarioModel> tablaMenuInventario){
-        this.tablaMenuInventario = tablaMenuInventario;
+    public void setTablaMenuInventario(TableView<EventoDiarioModel> tablaMenuEvento){
+        this.tablaMenuEvento = tablaMenuEvento;
     }
-    public void setInventarioMenuController (InventarioMenuController inventarioMenuController){
-        this.inventarioMenuController = inventarioMenuController;
+    public void setEventoDiarioController (EventoDiarioController eventoDiarioController){
+        this.eventoDiarioController = eventoDiarioController;
     }
 
     @FXML
     private void handleConfirmarModificacion(ActionEvent event){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmar Modificación");
-        alert.setHeaderText("Está a punto de modificar los datos del armamento.");
+        alert.setHeaderText("Está a punto de modificar los datos del evento.");
         alert.setContentText("¿Está seguro de que desea continuar?");
 
         ButtonType buttonTypeConfirmar = new ButtonType("Confirmar");
@@ -71,12 +74,12 @@ public class ModificarEventoController {
         Optional<ButtonType> result = alert.showAndWait();
 
         if (result.isPresent() && result.get() == buttonTypeConfirmar){
-            InventarioModel updateInventario = obtenerDatosFormulario();
+            EventoDiarioModel updateEvento = obtenerDatosFormulario();
 
-            if(updateInventario != null) {
-                InventarioDAO inventarioDAO = new InventarioDAO();
+            if(updateEvento != null) {
+                EventoDAO eventoDAO = new EventoDAO();
 
-                boolean exito = inventarioDAO.actualizarInventario(updateInventario);
+                boolean exito = eventoDAO.actualizarEventoDiario(updateEvento);
 
                 if (exito) {
                     mostrarAlerta("Actualización Exitosa", "Los datos del arma han sido actualizados correctamente.");
