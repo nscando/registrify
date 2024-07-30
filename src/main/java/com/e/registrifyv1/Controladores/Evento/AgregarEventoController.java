45ccf24aa7b467d072b44c5d57206f46f78c8d73
Patricio Ghirardi <ghirardi.patricio.w@gmail.com>
2024-07-30 01:14:36 -0300

    ComboBox en Agregado de Unidad y Gendarme para todos los menus correspondientes

--- src/main/java/com/e/registrifyv1/Controladores/Evento/AgregarEventoController.java
+++ src/main/java/com/e/registrifyv1/Controladores/Evento/AgregarEventoController.java
@@ -2,16 +2,26 @@ package com.e.registrifyv1.Controladores.Evento;
 
 import com.e.registrifyv1.Dao.EventoDAO;
 import com.e.registrifyv1.Dao.InventarioDAO;
+import com.e.registrifyv1.Dao.UsuarioDAO;
 import com.e.registrifyv1.Modelos.EventoDiario.EventoDiarioModel;
+import com.e.registrifyv1.Modelos.Unidad.UnidadMenuModel;
+import com.e.registrifyv1.Modelos.Usuarios.UsuarioModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class  AgregarEventoController implements Initializable {
@@ -20,10 +30,12 @@ public class  AgregarEventoController implements Initializable {
     @FXML
     private Button btnCancelar;
 
+    /*@FXML
+    private TextField txbIdGendarme;*/
     @FXML
-    private TextField txbIdGendarme;
+    private ComboBox<String> comboGendarme;
     @FXML
-    private TextField txbIdUnidad;
+    private ComboBox<String> comboUnidad;
     @FXML
     private TextField txbDescripcionEvento;
     @FXML
@@ -32,9 +44,38 @@ public class  AgregarEventoController implements Initializable {
     private TextField txbEstadoEvento;
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    private Map<String, Integer> unidadMap = new HashMap<>();
+    private Map<String, Integer> gendarmeMap = new HashMap<>();
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        try {
+            List<UnidadMenuModel> unidades = usuarioDAO.obtenerOpcionesUnidad();
+            ObservableList<String> unidadesNombres = FXCollections.observableArrayList();
+
+            for (UnidadMenuModel unidad : unidades) {
+                unidadMap.put(unidad.getNombreUnidad(), unidad.getIdUnidad());
+                unidadesNombres.add(unidad.getNombreUnidad());
+            }
+
+            comboUnidad.setItems(unidadesNombres);
+            comboUnidad.setPromptText("----Seleccione Unidad----");
+
+
+            List<UsuarioModel> usuarios = usuarioDAO.buscarUsuariosActivos();
+            ObservableList<String> usuariosNombres = FXCollections.observableArrayList();
+
+            for (UsuarioModel usuario : usuarios) {
+                gendarmeMap.put(usuario.getNombre() + " " + usuario.getApellido() + " " + usuario.getDni(), usuario.getIdGendarme());
+                usuariosNombres.add(usuario.getNombre() + " " + usuario.getApellido() + " " + usuario.getDni());
+            }
+            comboGendarme.setItems(usuariosNombres);
+            comboGendarme.setPromptText("----Seleccione Gendarme----");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     @FXML
     private void handleCancelarButtonAction(ActionEvent event) {
@@ -45,8 +86,17 @@ public class  AgregarEventoController implements Initializable {
     public void handleConfirmarButton(ActionEvent event) {
 
         int idEvento = 0;
-        int idUnidad = Integer.parseInt(txbIdUnidad.getText());
-        int idGendarme = Integer.parseInt(txbIdGendarme.getText());
+        String gendarmeSeleccionado = comboGendarme.getSelectionModel().getSelectedItem();
+        String unidadSeleccionada = comboUnidad.getSelectionModel().getSelectedItem();
+
+        if (gendarmeSeleccionado == null || unidadSeleccionada == null) {
+            mostrarMensaje(false, "Por favor seleccione un gendarme y una unidad.");
+            return;
+        }
+
+        int idGendarme = gendarmeMap.get(gendarmeSeleccionado);
+        int idUnidad = unidadMap.get(unidadSeleccionada);
+
         String descripcion = txbDescripcionEvento.getText();
         String fecha = txbFechaEvento.getText();
         int estado = Integer.parseInt(txbEstadoEvento.getText());
@@ -66,6 +116,27 @@ public class  AgregarEventoController implements Initializable {
         boolean carga = eventoDAO1.insertarEvento(nuevoEvento);
         mostrarMensaje(carga);
     }
+
+    private void mostrarMensaje(boolean exito, String mensaje) {
+        Alert alert = new Alert(exito ? Alert.AlertType.INFORMATION : Alert.AlertType.ERROR);
+        alert.setTitle(exito ? "Éxito" : "Error");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+
+        if (exito) {
+            comboGendarme.getSelectionModel().clearSelection();
+            comboUnidad.getSelectionModel().clearSelection();
+            comboGendarme.setPromptText("----Seleccione Gendarme----");
+            comboUnidad.setPromptText("----Seleccione Unidad----");
+
+        }
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.setAlwaysOnTop(true);
+
+        alert.showAndWait();
+    }
+
     private void mostrarMensaje(boolean carga) {
         Alert alert = new Alert(carga ? Alert.AlertType.INFORMATION : Alert.AlertType.ERROR);
         alert.setTitle(carga ? "Éxito" : "Error");
@@ -73,8 +144,6 @@ public class  AgregarEventoController implements Initializable {
 
         if (carga) {
             alert.setContentText("El Objeto se insertó correctamente.");
-            txbIdUnidad.clear();
-            txbIdGendarme.clear();
             txbDescripcionEvento.clear();
             txbFechaEvento.clear();
             txbEstadoEvento.clear();
--- src/main/java/com/e/registrifyv1/Controladores/Inventario/AgregarInventarioController.java
+++ src/main/java/com/e/registrifyv1/Controladores/Inventario/AgregarInventarioController.java
@@ -1,16 +1,26 @@
 package com.e.registrifyv1.Controladores.Inventario;
 
 import com.e.registrifyv1.Dao.InventarioDAO;
+import com.e.registrifyv1.Dao.UsuarioDAO;
 import com.e.registrifyv1.Modelos.Inventario.InventarioModel;
+import com.e.registrifyv1.Modelos.Unidad.UnidadMenuModel;
+import com.e.registrifyv1.Modelos.Usuarios.UsuarioModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class  AgregarInventarioController implements Initializable {
@@ -21,19 +31,49 @@ public class  AgregarInventarioController implements Initializable {
 
     @FXML
     private TextField txbIdAccesorio;
-    @FXML
-    private TextField txbIdUnidad;
-    @FXML
-    private TextField txbIdGendarme;
+
     @FXML
     private TextField txbNombre;
     @FXML
     private TextField txbDescripcion;
+    @FXML
+    private ComboBox<String> comboGendarme;
+    @FXML
+    private ComboBox<String> comboUnidad;
 
+
+    private Map<String, Integer> unidadMap = new HashMap<>();
+    private Map<String, Integer> gendarmeMap = new HashMap<>();
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        try {
+            List<UnidadMenuModel> unidades = usuarioDAO.obtenerOpcionesUnidad();
+            ObservableList<String> unidadesNombres = FXCollections.observableArrayList();
 
+            for (UnidadMenuModel unidad : unidades) {
+                unidadMap.put(unidad.getNombreUnidad(), unidad.getIdUnidad());
+                unidadesNombres.add(unidad.getNombreUnidad());
+            }
+
+            comboUnidad.setItems(unidadesNombres);
+            comboUnidad.setPromptText("----Seleccione Unidad----");
+
+
+            List<UsuarioModel> usuarios = usuarioDAO.buscarUsuariosActivos();
+            ObservableList<String> usuariosNombres = FXCollections.observableArrayList();
+
+            for (UsuarioModel usuario : usuarios) {
+                gendarmeMap.put(usuario.getNombre() + " " + usuario.getApellido() + " " + usuario.getDni(), usuario.getIdGendarme());
+                usuariosNombres.add(usuario.getNombre() + " " + usuario.getApellido() + " " + usuario.getDni());
+            }
+            comboGendarme.setItems(usuariosNombres);
+            comboGendarme.setPromptText("----Seleccione Gendarme----");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     @FXML
     private void handleCancelarButtonAction(ActionEvent event) {
@@ -44,8 +84,16 @@ public class  AgregarInventarioController implements Initializable {
     public void handleConfirmarButton(ActionEvent event) {
 
         int idAccesorio = 0;
-        int idUnidad = Integer.parseInt(txbIdUnidad.getText());
-        int idGendarme = Integer.parseInt(txbIdGendarme.getText());
+        String gendarmeSeleccionado = comboGendarme.getSelectionModel().getSelectedItem();
+        String unidadSeleccionada = comboUnidad.getSelectionModel().getSelectedItem();
+
+        if (gendarmeSeleccionado == null || unidadSeleccionada == null) {
+            mostrarMensaje(false, "Por favor seleccione un gendarme y una unidad.");
+            return;
+        }
+
+        int idGendarme = gendarmeMap.get(gendarmeSeleccionado);
+        int idUnidad = unidadMap.get(unidadSeleccionada);
         String nombre = txbNombre.getText();
         String descripcion = txbDescripcion.getText();
 
@@ -69,8 +117,6 @@ public class  AgregarInventarioController implements Initializable {
 
         if (carga) {
             alert.setContentText("El Objeto se insertó correctamente.");
-            txbIdUnidad.clear();
-            txbIdGendarme.clear();
             txbNombre.clear();
             txbDescripcion.clear();
 
@@ -79,6 +125,25 @@ public class  AgregarInventarioController implements Initializable {
         }
 
         // Configurar el cuadro de diálogo para que sea siempre visible
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.setAlwaysOnTop(true);
+
+        alert.showAndWait();
+    }
+    private void mostrarMensaje(boolean exito, String mensaje) {
+        Alert alert = new Alert(exito ? Alert.AlertType.INFORMATION : Alert.AlertType.ERROR);
+        alert.setTitle(exito ? "Éxito" : "Error");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+
+        if (exito) {
+            comboGendarme.getSelectionModel().clearSelection();
+            comboUnidad.getSelectionModel().clearSelection();
+            comboGendarme.setPromptText("----Seleccione Gendarme----");
+            comboUnidad.setPromptText("----Seleccione Unidad----");
+
+        }
+
         Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
         stage.setAlwaysOnTop(true);
 
